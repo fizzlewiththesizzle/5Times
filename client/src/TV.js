7ebf4ff4131bc8fb25b35b2a7043fff5799cd83b
Faisal Islam <faisy@hotmail.ca>
2024-01-01 13:44:55 -0700

    add multipage with hashrouter

---
+++ client/src/TV.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function TV() {
+    return (
+        <React.Fragment>
+            <div>
+                <h1 className='dark: text-white'>TV Page</h1>
+            </div>
+            
+        </React.Fragment>
+    )
+}
+
+
+export default TV;
