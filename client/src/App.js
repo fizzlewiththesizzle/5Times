c03ec6cd18005d261d5bc7887a899a34b843d596
Faisal Islam <faisy@hotmail.ca>
2024-01-12 15:15:34 -0700

    add lazy loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,33 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import TV from './TV';
-import Month from './Month';
- 
- const App = () => (
-  <HashRouter>
-    <Routes>
-    <Route
-      exact
-      path='/'
-      element={<Home/>}
-    />
-    <Route
-      exact
-      path='/TV'
-      element={<TV/>}
-    />
-    <Route
-      exact
-      path='/Month'
-      element={<Month/>}
-    />
-    </Routes>
-  </HashRouter>
-)
+import './App.css';
 
-export default App
+const Home = lazy(() => import('./Home'));
+const TV = lazy(() => import('./TV'));
+const Month = lazy(() => import('./Month'));
+
+const App = () => (
+  <HashRouter>
+    <Suspense fallback={<div></div>}>
+      <Routes>
+        <Route
+          exact
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          exact
+          path='/TV'
+          element={<TV />}
+        />
+        <Route
+          exact
+          path='/Month'
+          element={<Month />}
+        />
+      </Routes>
+    </Suspense>
+  </HashRouter>
+);
+
+export default App;
