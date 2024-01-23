45195751e39f6399f5a3e51bfb3a74623bd54151
Faisal Islam <faisy@hotmail.ca>
2024-01-23 08:54:46 -0700

    add speed insights

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 import Home from './Home';
 import TV from './TV';
 import Month from './Month';
 
 inject();
+injectSpeedInsights();
 
 const App = () => (
   <HashRouter>
